652a30aff0eb37645f73d1649f9d0a36e8e85853
Kiran G <mail@kirang.me>
2022-03-26 15:19:40 +0530

    initial

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,143 @@
+import {
+  EmailOutlined,
+  Facebook,
+  Instagram,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Paymnet = styled.img`
+  width: 50%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>ECOM.</Logo>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
+          iusto, quas enim eveniet reprehenderit accusantium culpa debitis neque
+          ut possimus officia nobis optio animi necessitatibus odio excepturi
+          facere distinctio? Itaque?
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Men Fashion</ListItem>
+          <ListItem>Women Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Track Order</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>About Us</ListItem>
+          <ListItem>Contact Us</ListItem>
+          <ListItem>Privacy Policy</ListItem>
+          <ListItem>Terms of Use</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} />
+          15, Sector 4, HSR Layout, Bnagalore - 560102
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +91 9880098800
+        </ContactItem>
+        <ContactItem>
+          <EmailOutlined style={{ marginRight: "10px" }} /> contact@ecom.in
+        </ContactItem>
+        <Paymnet src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
